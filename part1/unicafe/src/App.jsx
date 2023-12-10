00074d37e492d3c178689ae2f765f9937aff527e
Leonardo Aceituno <leonardo.aceituno@gmail.com>
2023-12-10 19:50:58 -0300

    exercises 1.6.-1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,36 +1,74 @@
+import { useState } from "react";
+
+const StatisticLine = ({ text, value, simbol = " " }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>
+        {Number.isInteger(value) ? value : value.toFixed(2)} {simbol}
+      </td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total / 3;
+  const positive = (good * 100) / total;
+
+  if (total == 0)
+    return (
+      <>
+        <h3>Statistics</h3>
+        <span>No feedback given</span>
+      </>
+    );
+  else
+    return (
+      <>
+        <h3>Statistics</h3>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
+            <StatisticLine text="All" value={total} />
+            <StatisticLine text="Average" value={average} />
+            <StatisticLine
+              text="Positive"
+              value={good == 0 ? 0 : positive}
+              simbol="%"
+            />
+          </tbody>
+        </table>
+      </>
+    );
+};
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.handleClick}>{props.text}</button> &nbsp;
+    </>
+  );
+};
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const partes = parts.map((value, index) => (
-    <p key={index}>
-      {value.name} {value.exercises}
-    </p>
-  ));
-
-  const totalExercises = () =>
-    parts.reduce((accumulator, value) => {
-      return accumulator + value.exercises;
-    }, 0);
+  const handleGoodButton = () => setGood(good + 1);
+  const handleNeutralButton = () => setNeutral(neutral + 1);
+  const handleBadButton = () => setBad(bad + 1);
 
   return (
     <>
-      <h1>{course}</h1>
-      <div>{partes}</div>
-      <p>Number of exercises: {totalExercises()}</p>
+      <h2>Give feedback</h2>
+      <Button handleClick={handleGoodButton} text="Good" />
+      <Button handleClick={handleNeutralButton} text="Neutral" />
+      <Button handleClick={handleBadButton} text="Bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />{" "}
     </>
   );
 };
